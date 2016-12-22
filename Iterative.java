b05fbb4b7bd4b59aa173e01faaa468af73453a1c
MatthewHorn <matthew.horn@wmtps.net>
2016-12-22 12:16:49 -0500

    Create Iterative.java

---
+++ Iterative.java
@@ -0,0 +1,46 @@
+
+public class Iterative {
+
+	/**factorial
+	 * @param int n
+	 * AT MOST ONE LOCAL VARIABLE
+	 *  returns the factorial of n 
+	 */
+
+	/**afactorial
+	 * @param int n
+	 * AT MOST TWO LOCAL VARIABLES
+	 *  returns the factorial of n after printing the numbers to be
+	 *  multiplied in ascending order 
+	 *  example: 1 * 2 * 3 * 4 * 5
+	 */
+
+	/**dfactorial
+	 * @param int n
+	 * AT MOST ONE LOCAL VARIABLE
+	 *  returns the factorial of n after printing the numbers to be
+	 *  multiplied in descending order 
+  	 *  example: 5 * 4 * 3 * 2 * 1
+	 */
+
+	/**intBasesToIntPowers
+	 * @param int b, int exp
+	 * AT MOST ONE LOCAL VARIABLE
+	 *  returns the value of the base (b) to the exp (exp) 
+	 */
+	
+	/**fibonacciSequence
+	 * @param int s, int n
+	 * NO LIMIT (WITHIN REASON) ON LOCAL VARIABLE
+	 *  prints out the fibonacci sequence with a start value of s and 
+	 *  provides the first n terms 
+	 */
+	
+	/**fibonacciSeries
+	 * @param int s, int n
+	 * NO LIMIT (WITHIN REASON) ON LOCAL VARIABLE
+	 *  returns the sums the first n digits of a fibonacci sequence 
+	 *  with a start value of s 
+	 */
+	
+}
