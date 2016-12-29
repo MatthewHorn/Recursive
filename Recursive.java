e348f282038ca38da7955f37e60d91227db99c29
MatthewHorn <matthew.horn@wmtps.net>
2016-12-29 00:47:35 -0500

    Update Recursive.java

--- Recursive.java
+++ Recursive.java
@@ -19,12 +19,12 @@ public class Recursive {
   	 *  example: 1 * 2 * 3 * 4 * 5
 	 */
 	public int afactorial(int n, int m) {
-		if (n == m) {
-			System.out.printf("%d", n);
-			return n;
+		if (n == 1) {
+			System.out.printf("%d", m);
+			return 1;
 		} else {
-			System.out.printf(" * %d", n);
-			return n * afactorial(n - 1, m);
+			System.out.printf("%d * ", m);
+			return n * afactorial(n - 1, m + 1);
 		}
 	}
 		
@@ -35,12 +35,27 @@ public class Recursive {
 	 *  multiplied in descending order 
 	 *  example: 5 * 4 * 3 * 2 * 1
 	 */
+	public int dfactorial(int n) {
+		if (n == 1) {
+			System.out.print('1');
+			return 1;
+		} else {
+			System.out.printf("%d * ", n);
+			return n * dfactorial(n - 1);
+		}
+	}
 	
 	/**intBasesToIntPowers
 	 * @param int b, int exp
 	 * NO LOCAL VARIABLE
 	 *  returns the value of the base (b) to the exp (exp) 
 	 */
+	public int intBasesToIntPowers(int b, int exp) {
+		if (exp == 1)
+			return b;
+		else
+			return b * intBasesToIntPowers(b, exp - 1);
+	}
 		
 	/**fibonacciSequence
 	 * @param NO LIMIT (WITHIN REASON)
@@ -49,6 +64,11 @@ public class Recursive {
 	 *  print out the fibonacci sequence with a start value of s and 
 	 *  provides the first n terms 
 	 */
+	public int fibonacciSequence(int s, int n) {
+		// "since all iterative functions can be done recursively..."
+		// [citation needed]
+	
+	}
 
 	/**fibonacciSeries
 	 * @param int s, int n
