b805d21013fc2d57073714b2db9b739bdc3170b7
MatthewHorn <matthew.horn@wmtps.net>
2017-01-13 11:42:08 -0500

    Update Recursive.java

--- Recursive.java
+++ Recursive.java
@@ -19,11 +19,13 @@ public class Recursive {
   	 *  example: 1 * 2 * 3 * 4 * 5
 	 */
 	public int afactorial(int n, int m) {
-		if (n == 0) {
-			System.out.printf("%d", m);
+		if (n == 0 && m == 1) {
+			System.out.print("1");
 			return 1;
-		} else {
-			System.out.printf("%d * ", m);
+		} else if (n == 0)
+			return 1;
+		else {
+			System.out.printf("%d%s", m, n == 1 ? "" : " * ");
 			return n * afactorial(n - 1, m + 1);
 		}
 	}
@@ -36,7 +38,7 @@ public class Recursive {
 	 *  example: 5 * 4 * 3 * 2 * 1
 	 */
 	public int dfactorial(int n) {
-		if (n == 1) {
+		if (n == 1 || n == 0) {
 			System.out.print('1');
 			return 1;
 		} else {
@@ -51,8 +53,8 @@ public class Recursive {
 	 *  returns the value of the base (b) to the exp (exp) 
 	 */
 	public int intBasesToIntPowers(int b, int exp) {
-		if (exp == 1)
-			return b;
+		if (exp == 0)
+			return 1;
 		else
 			return b * intBasesToIntPowers(b, exp - 1);
 	}
@@ -65,10 +67,12 @@ public class Recursive {
 	 *  provides the first n terms 
 	 */
 	public void fibonacciSequence(int s, int r, int n) {
-		// "since all iterative functions can be done recursively..."
-		// Doubt it.
-		System.out.printf("%d%s", r, n == 1 ? "\n" : ", ");
-		if (n > 1) fibonacciSequence(r, s + r, n - 1);
+		if (n == 0);
+		else if (n == 1) System.out.printf("%d", s);
+		else {
+			System.out.printf("%d, ", s);
+			fibonacciSequence(r, s + r, n - 1);
+		}
 	}
 
 	/**fibonacciSeries
